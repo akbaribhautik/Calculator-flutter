b34ad01b5446393c70f22776238fb8dc2cc9cbf4
Bhautik Akbari <akbaribhautik32@gmail.com>
2020-02-03 17:03:12 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+
+      title: "Calculator",
+      
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int firstnum;
+  int secondnum;
+  String texttodisplay = "";
+  String res;
+  String operatortoperform;
+  void btnclicked(String btnval){
+    if(btnval == "C"){
+      texttodisplay = "";
+      firstnum = 0;
+      secondnum = 0;
+      res = "";
+
+    }
+    else if(btnval == "+" || btnval == "-" || btnval == "*" || btnval == "/" ){
+      firstnum = int.parse(texttodisplay);
+      res = "";
+      operatortoperform = btnval;
+
+    }
+    else if(btnval == "="){
+      secondnum = int.parse(texttodisplay);
+        
+      if(operatortoperform == "+"){
+        res = (firstnum + secondnum).toString();
+      }
+      if(operatortoperform == "-"){
+        res = (firstnum - secondnum).toString();
+
+    }
+    if(operatortoperform == "*"){
+        res = (firstnum * secondnum).toString();
+  }
+  if(operatortoperform == "/"){
+        res = (firstnum / secondnum).toString();
+    }
+    }
+    else{
+      res = int.parse(texttodisplay + btnval).toString();
+          }
+          setState(() {
+            texttodisplay = res;
+          });
+  }
+  Widget custombutton(String btnval){
+     
+    return Expanded(
+      child: OutlineButton(
+       
+        padding: EdgeInsets.all(25.0),
+        onPressed: () => btnclicked(btnval),
+        child: Text(
+          "$btnval",
+          style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    
+                     
+                  ),
+        
+          ),
+        ),
+        );
+      
+    
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        title:Text(
+         "Calculator", 
+        
+           
+         
+        )
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue,
+        ),
+        alignment: Alignment.bottomRight,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  "$texttodisplay",
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                
+                custombutton("7"),
+                custombutton("8"),
+                custombutton("9"),
+                custombutton("/"),
+
+              ],
+            ),
+             Row(
+              children: <Widget>[
+                custombutton("4"),
+                custombutton("5"),
+                custombutton("6"),
+                custombutton("*"),
+
+              ],
+            ),
+             Row(
+              children: <Widget>[
+                custombutton("1"),
+                custombutton("2"),
+                custombutton("3"),
+                custombutton("-"),
+
+              ],
+            ),
+             Row(
+              children: <Widget>[
+                custombutton("C"),
+                custombutton("0"),
+                custombutton("="),
+                custombutton("+"),
+
+              ],
+            ),
+          ],
+        ) ,
+
+      ),
+      
+    );
+  }
+}
